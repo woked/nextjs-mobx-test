30ff36863bfce795deaf8574ff89aca96617b0d6
yami <imay@yami.com>
2021-06-25 16:36:49 +0000

    settings project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+};
