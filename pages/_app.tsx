9d31aa6db515813c0afaa6d6b72162ac9d0f15ef
yami <imay@yami.com>
2021-06-26 13:57:15 +0000

    add enableStaticRendering for ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import {enableStaticRendering} from 'mobx-react-lite';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+if (!process.browser) {
+  enableStaticRendering(true);
 }
-export default MyApp
+
+function MyApp({Component, pageProps}: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
