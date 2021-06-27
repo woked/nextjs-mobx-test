d703ae777143f96141b6088073e2963445e1610f
yami <imay@yami.com>
2021-06-27 17:28:29 +0000

    add HOST constant

--- src/libs/http/index.ts
+++ src/libs/http/index.ts
@@ -1,5 +1,8 @@
 import axios, {AxiosInstance} from 'axios';
+import {HOST} from 'constants/index';
 
 export type HttpType = AxiosInstance;
 
-export const http: HttpType = axios.create();
+export const http: HttpType = axios.create({
+  baseURL: process.browser ? '' : HOST,
+});
