ca5f4c540a786594a47745ff7b434968ab835042
yami <imay@yami.com>
2021-06-27 18:28:31 +0000

    implement pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {enableStaticRendering} from 'mobx-react-lite';
+import {RootStoreProvider} from 'store/context';
+import {RootStoreHydrationType} from 'store/root';
 
 if (!process.browser) {
   enableStaticRendering(true);
 }
 
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+export type PagePropsType = {state: RootStoreHydrationType};
+
+function MyApp({
+  Component,
+  pageProps,
+}: Omit<AppProps<PagePropsType>, 'pageProps'> & {
+  pageProps: PagePropsType;
+}) {
+  return (
+    <RootStoreProvider hydrationData={pageProps.state}>
+      <Component {...pageProps} />
+    </RootStoreProvider>
+  );
 }
 
 export default MyApp;
